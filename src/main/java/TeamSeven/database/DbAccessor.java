9cc786587cd97a1290310ca4ac91ec7236c32707
Josh Ouyang <JoshOY789@gmail.com>
2016-05-25 00:53:04 +0800

    db accessor update

--- src/main/java/TeamSeven/database/DbAccessor.java
+++ src/main/java/TeamSeven/database/DbAccessor.java
@@ -1,7 +1,31 @@
 package TeamSeven.database;
 
+import TeamSeven.util.config.ConfigManager;
+import TeamSeven.util.config.ConfigManagerImpl;
+
+import java.sql.*;
+
 /**
  * Created by joshoy on 16/4/17.
  */
-public interface DbAccessor {
+public abstract class DbAccessor {
+
+    protected static ConfigManager cm = new ConfigManagerImpl("demoServerConfig.conf");
+
+    // JDBC driver name and database URL
+    protected static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    protected static final String DB_URL = "jdbc:mysql://" + cm.getString("database.host") + ":" + cm.getString("database.port") + "/" + cm.getString("database.dbName");
+    //  Database credentials
+    protected static final String USERNAME = cm.getString("database.username");
+    protected static final String PASSWORD = cm.getString("database.password");
+    protected static Connection dbConn;
+    protected static Statement stmt;
+
+    public abstract ResultSet executeSQLQueryStatement(String sql) throws SQLException;
+    public abstract void executeSQLUpdateStatement(String sql) throws SQLException;
+    public abstract void executeSQLStatement(String sql) throws SQLException;
+
+    public abstract void endStatement();
+
+    public abstract void endConnection();
 }
--- src/main/java/TeamSeven/database/DbAccessorImpl.java
+++ src/main/java/TeamSeven/database/DbAccessorImpl.java
@@ -1,7 +1,89 @@
 package TeamSeven.database;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by joshoy on 16/4/17.
  */
-public class DbAccessorImpl implements DbAccessor {
+public class DbAccessorImpl extends DbAccessor {
+
+    public DbAccessorImpl() {
+        try {
+            //  Register JDBC driver
+            Class.forName(this.JDBC_DRIVER);
+            System.out.println("Connecting to database...");
+            this.dbConn = DriverManager.getConnection(this.DB_URL, this.USERNAME, this.PASSWORD);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public ResultSet executeSQLQueryStatement(String sql) throws SQLException {
+        if (this.stmt == null) {
+            stmt = this.dbConn.createStatement();
+        }
+        else {
+            this.stmt.close();
+            stmt = this.dbConn.createStatement();
+        }
+        ResultSet rs = this.stmt.executeQuery(sql);
+        return rs;
+    }
+
+    @Override
+    public void executeSQLUpdateStatement(String sql) throws SQLException {
+        if (this.stmt == null) {
+            stmt = this.dbConn.createStatement();
+        }
+        else {
+            this.stmt.close();
+            stmt = this.dbConn.createStatement();
+        }
+        stmt.executeUpdate(sql);
+    }
+
+    @Override
+    public void executeSQLStatement(String sql) throws SQLException {
+        if (this.stmt == null) {
+            stmt = this.dbConn.createStatement();
+        }
+        else {
+            this.stmt.close();
+            stmt = this.dbConn.createStatement();
+        }
+        stmt.execute(sql);
+    }
+
+    @Override
+    public void endStatement() {
+        if (this.stmt != null) {
+            try {
+                this.stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            this.stmt = null;
+        }
+    }
+
+    @Override
+    public void endConnection() {
+        if (this.stmt != null) {
+            this.endStatement();
+        }
+        if (this.dbConn != null) {
+            try {
+                dbConn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            dbConn = null;
+        }
+    }
+
 }
